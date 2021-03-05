8886892fec200e4e062653f88143a9f211bfa282
Carlos <guerreroc47@gmail.com>
2021-03-04 18:17:12 -0800

    update it

--- source/alien_invasion.py
+++ source/alien_invasion.py
@@ -11,24 +11,53 @@ class AlienInvasion:
         # Initialize the game, and create game resources
         pg.init()
         self.settings = Settings()
-        self.screen = pg.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pg.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
     def run_game(self):
         # Start the main loop for the game
         while True:
-            # watch for keyboard and mouse events
-            for event in pg.event.get():
-                if event.type == pg.quit:
-                    sys.exit()
+            self._check_events()  # refactor the original code
+            # self.ship_update()
+            self._update_screen()  # refactor the original code
 
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # make the most recently drawn screen visible
-            pg.display.flip()
+    def _check_events(self):
+        """ Respond to keypresses and mouse events"""
+        for event in pg.event.get():
+            if event.type == pg.quit:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pg.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """ Respond to keypresses."""
+        if event.key == pg.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pg.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pg.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """ Respind to key releases """
+        if event.key == pg.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pg.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """ Update images on the screen, and flip to the new screen.  """
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # make the most recently drawn screen visible
+        pg.display.flip()
 
 
 if __name__ == '__main__':
