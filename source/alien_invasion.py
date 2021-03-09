2dfe15dc1afcae532972a7615265d3c8d9e83797
Carlos <guerreroc47@gmail.com>
2021-03-08 21:08:57 -0800

    implement the bullets feature

--- source/alien_invasion.py
+++ source/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame as pg
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -16,13 +17,25 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pg.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pg.sprite.Group()
 
     def run_game(self):
         # Start the main loop for the game
         while True:
             self._check_events()  # refactor the original code
             # self.ship_update()
+            self._update_bullets()
             self._update_screen()  # refactor the original code
+
+    def _update_bullets(self):
+        """ Update position of bullets and get rid of old bullets """
+        # Update bullet positions
+        self.bullets.update()
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _check_events(self):
         """ Respond to keypresses and mouse events"""
@@ -43,6 +56,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pg.K_q:
             sys.exit()
+        elif event.key == pg.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """ Respind to key releases """
@@ -51,11 +66,19 @@ class AlienInvasion:
         elif event.key == pg.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """ Create a new bullet and add it to the bullets group. """
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """ Update images on the screen, and flip to the new screen.  """
         # Redraw the screen during each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # make the most recently drawn screen visible
         pg.display.flip()
 
